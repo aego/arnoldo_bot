d4a7f361a9ee3f83d3cd9e819f182003e911f5c2
aezhko <alexkondrashin4@gmail.com>
2022-11-29 02:12:59 +0300

    project init

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.21"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.8.0-Beta"
+    application
+}
+
+group = "ru.aezhko"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "arnoldo_bot"
+
