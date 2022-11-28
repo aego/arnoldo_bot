7701d0f57ca6af4a1dcf5f04e59a2158b4e51497
aezhko <alexkondrashin4@gmail.com>
2022-11-29 02:32:25 +0300

    spring app init

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.8.0-Beta"
+//    id("org.jetbrains.kotlin.plugin.spring") version "1.8.0-Beta"
+    id("org.springframework.boot") version "3.0.0"
+    kotlin("plugin.jpa") version "1.6.21"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("plugin.spring") version "1.6.21"
     application
 }
 
@@ -14,7 +18,16 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-mustache")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation(kotlin("test"))
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
@@ -23,6 +36,8 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
+    kotlinOptions.freeCompilerArgs = listOf("-Xjsr305=strict")
+
 }
 
 application {
---
+++ src/main/kotlin/ru/aezhko/BotApplication.kt
@@ -0,0 +1,10 @@
+package ru.aezhko
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BotApplication
+fun main(args: Array<String>) {
+    runApplication<BotApplication>(*args)
+}
